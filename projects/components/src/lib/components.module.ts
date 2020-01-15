c1ffb3b739ae6171a7a855248f5a54e682f3e9e5
Juan Manuel Zaragoza <jzaragoza@fi.uba.ar>
2020-01-15 14:07:07 -0300

    Fixed table-filtered component

--- projects/components/src/lib/components.module.ts
+++ projects/components/src/lib/components.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ComponentsComponent } from './components.component';
+import { TableFilteredComponent } from './table-filtered/table-filtered.component';
 
 
 
 @NgModule({
-  declarations: [ComponentsComponent],
+  declarations: [ComponentsComponent, TableFilteredComponent],
   imports: [
   ],
-  exports: [ComponentsComponent]
+  exports: [ComponentsComponent, TableFilteredComponent]
 })
 export class ComponentsModule { }
